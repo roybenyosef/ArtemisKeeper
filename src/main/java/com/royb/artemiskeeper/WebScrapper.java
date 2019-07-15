a37650216066be33b2ad0d1a9bc618de678e3c52
roy by <roybenyosef@gmail.com>
2019-07-16 01:29:06 +0300

    working on webscraper

--- src/main/java/com/royb/artemiskeeper/WebScrapper.java
+++ src/main/java/com/royb/artemiskeeper/WebScrapper.java
@@ -4,10 +4,11 @@ import com.royb.artemiskeeper.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-
+@Component
 public class WebScrapper {
 
     //TODO - log to file
